125144ce8406722292368fd436d8a8cf54be7abc
lanboys <lan_bing2013@163.com>
2022-07-16 23:44:16 +0800

    调整包

--- ribbon-java/src/main/java/com/bing/lan/RibbonClient.java
+++ ribbon-java/src/main/java/com/bing/lan/RibbonClient.java
@@ -1,11 +1,13 @@
 package com.bing.lan;
 
 import com.netflix.client.ClientFactory;
+import com.netflix.client.IClient;
 import com.netflix.config.ConfigurationManager;
 import com.netflix.loadbalancer.RoundRobinRule;
 import com.netflix.loadbalancer.ILoadBalancer;
 import com.netflix.loadbalancer.IRule;
 import com.netflix.loadbalancer.Server;
+
 
 /**
  * Created by backend.
@@ -14,13 +16,22 @@ import com.netflix.loadbalancer.Server;
 public class RibbonClient {
 
   public static void main(String[] args) throws Exception {
+    // 加载配置文件
     ConfigurationManager.loadCascadedPropertiesFromResources("ribbon-client");
+
     ILoadBalancer loadBalancer = ClientFactory.getNamedLoadBalancer("MyRibbonClient");
     IRule chooseRule = new RoundRobinRule();
     chooseRule.setLoadBalancer(loadBalancer);
     for (int i = 0; i < 10; i++) {
       Server server = chooseRule.choose(null);
-      //System.out.println("request" + server.getHostPort());
+      System.out.println("request ：" + server.getHostPort());
+    }
+
+    System.out.println("main(): ============");
+
+    for (int i = 0; i < 10; i++) {
+      Server server = loadBalancer.chooseServer(null);
+      System.out.println("request ：" + server.getHostPort());
     }
   }
 }
---
+++ ribbon-java/src/test/java/com/bing/lan/config/ConfigurationManagerTest.java
@@ -0,0 +1,7 @@
+package com.bing.lan.config;
+/**
+ * Created by oopcoder at 2022/7/17 8:52 .
+ */
+
+   class ConfigurationManagerTest   {
+}
